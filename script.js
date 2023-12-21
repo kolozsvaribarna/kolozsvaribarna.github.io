fbcb1f88aaf79ae2db06f8dfab1f78ac042d1010
azigazibarni <kolozsvaribarnaa@gmail.com>
2023-12-21 09:20:46 +0100

    simplified #2

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 var i = 0;
 var txt = "Barna";
 var speed = 150;
-const body = document.getElementsByTagName("body")[0];
-const arrow = document.getElementsByClassName('arrow')[0];
-
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
-
-body.setAttribute('class', 'stop-scrolling');
-arrow.style.opacity = 0;
-arrow.style.transition = "opacity 1s";
 
 (function typeWriter() {
     if (i < txt.length) {
@@ -19,8 +9,3 @@ arrow.style.transition = "opacity 1s";
         setTimeout(typeWriter, speed);
     }
 })();
-
-setTimeout(() => {
-    arrow.style.opacity = 1; /*show arrows*/
-    body.removeAttribute("class"); /*enable scrolling*/
-}, speed * txt.length + 450);
