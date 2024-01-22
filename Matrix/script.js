aaa522afa18f50bc6ea1c3459fea38e15cda36fd
azigazibarni <kolozsvaribarnaa@gmail.com>
2024-01-22 18:29:10 +0100

    linked eisenhower Matrix

---
+++ Matrix/script.js
@@ -0,0 +1,87 @@
+let list_items = document.getElementsByClassName('list_item');
+let box1 = document.getElementById('box_1');
+let box2 = document.getElementById('box_2');
+let box3 = document.getElementById('box_3');
+let box4 = document.getElementById('box_4');
+let box5 = document.getElementById('box_5');
+
+function itemDraggable() {
+    for(item of list_items) {
+    item.addEventListener('dragstart', function(e) {
+        let selected = e.target;
+        //box 1
+        box1.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        });
+        box1.addEventListener('drop', function() {
+            box1.appendChild(selected);
+            selected = null;
+        })
+        //box 2
+        box2.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        });
+        box2.addEventListener('drop', function() {
+            box2.appendChild(selected);
+            selected = null;
+        })
+        // box 3
+        box3.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        });
+        box3.addEventListener('drop', function() {
+            box3.appendChild(selected);
+            selected = null;
+        })
+        //box 4
+        box4.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        });
+        box4.addEventListener('drop', function() {
+            box4.appendChild(selected);
+            selected = null;
+        })
+        //box 5
+        box5.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        });
+        box5.addEventListener('drop', function() {
+            box5.appendChild(selected);
+            selected = null;
+        })
+    })
+}
+    const deleteButtons = document.querySelectorAll('.delButt');
+
+    function removeMe() {
+        // this is the button
+        // from the button you want to move up in DOM 
+        // and find the closes <li> to remove
+        this.closest('div').remove(); 
+    }
+
+    deleteButtons.forEach( button => {
+      // add the event listener to each button
+      button.addEventListener('click', removeMe); 
+    });
+}
+
+
+
+function addItem() {
+    let item = document.createElement('div');
+    let item_p = document.createElement('p');
+    let item_b = document.createElement('button');
+    item.className = 'list_item';
+    item.draggable = 'true';
+    item_p.innerHTML = 'Type here..';
+    item_p.setAttribute('contentEditable','true');
+    item.appendChild(item_p);
+    item_b.type = 'button';
+    item_b.className = 'delButt';
+    item_b.innerHTML = 'x';
+    item.appendChild(item_b)
+    box1.appendChild(item);
+
+    itemDraggable();
+}
