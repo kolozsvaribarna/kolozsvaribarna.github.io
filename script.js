680dd214faca304f675f2336da600eb1eefef0b6
azigazibarni <kolozsvaribarnaa@gmail.com>
2024-01-29 23:14:23 +0100

    animation on scroll

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ toggle.addEventListener('input', (e) => {
 		body.classList.toggle('light-theme');
 	}
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
