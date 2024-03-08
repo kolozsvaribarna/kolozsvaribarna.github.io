e505c5238880fdff070df8cd41867884ae7398f7
azigazibarni <kolozsvaribarnaa@gmail.com>
2024-03-08 20:53:46 +0100

    title update, added links

--- script.js
+++ script.js
@@ -2,26 +2,51 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
+window.onscroll = function() {addSticky()};
+let header = document.querySelectorAll('#title')[0];
+let sticky = header.offsetTop;
+function addSticky() {
+if (window.pageYOffset > sticky) {
+    header.classList.add("stickyHeader");
+  } else {
+    header.classList.remove("stickyHeader");
+  }
+}
+
+let toggle = document.getElementById('toggle');
 let i = 0;
 (function typeWriter() {
+    toggle.disabled = false;
     let txt = "barna";
     let speed = 280;
     if (i < txt.length) {
         document.getElementsByClassName('text')[0].innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
+        toggle.disabled = true;
     }
 })();
 
-let toggle = document.getElementById('toggle');
 let body = document.body;
+let a = document.getElementsByTagName('a');
+let title = document.getElementById('title');
+
 toggle.addEventListener('input', (e) => {
 	let isChecked = e.target.checked;
-	if(isChecked) {
-		body.classList.toggle('light-theme');
-	} else {
-		body.classList.toggle('light-theme');
-	}
+    if(isChecked) {
+        body.classList.toggle('light-theme');
+        title.classList.toggle('light-theme');
+        for (i=0; i<a.length; i++) {
+            a[i].classList.toggle('light-theme');
+        }
+    }
+    else {
+        body.classList.toggle('light-theme');
+        title.classList.toggle('light-theme');
+        for (i=0; i<a.length; i++) {
+            a[i].classList.toggle('light-theme');
+        }
+    }
 });
 
 const observer = new IntersectionObserver((entries) => {
@@ -29,9 +54,9 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
-        else {
-            entry.target.classList.remove('show');
-        }
+        //else {
+        //    entry.target.classList.remove('show');
+        //}
     })
 
 });
