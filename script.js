08c58894f787e1e37115f6f99e60c1e57239d780
azigazibarni <kolozsvaribarnaa@gmail.com>
2024-04-20 20:29:37 +0200

    fixed matrix link under 775px

--- script.js
+++ script.js
@@ -20,3 +20,14 @@
       }
     }
 })()
+;(function() {
+    if (window.screen.width <= 775) {
+        var links = document.querySelectorAll('.disable-small');
+        for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            link.addEventListener("click", function(e) {
+                e.preventDefault();
+            }, false);
+        }
+    }
+})()
